93a51abff2d80fb062f38471f7777585de8055bd
kevin.schu <kevin.schu@innovation4web.de>
2018-09-22 13:11:06 +0200

    Show warning only when module is not disabled

--- lib/puppet/functions/puppetstats/track.rb
+++ lib/puppet/functions/puppetstats/track.rb
@@ -19,63 +19,67 @@ Puppet::Functions.create_function(:'puppetstats::track') do
     rescue => error
       Puppet.send('warning', "Could not detect version for module '#{fqmn}' using '#{module_name}': #{error}")
     else
-      version = metadata['version']
-      if version.to_s.empty?
-        Puppet.send('warning', "The module '#{module_name}' has no metadata.json. puppetstats cannot analyse data without a module version.")
-      else
-        uri = URI.parse('https://puppetstats.com')
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-
-        request = Net::HTTP::Post.new("/api/modules/#{fqmn}")
-
-        scope = closure_scope
-        facts = scope['facts']
-
-        unique_identifier = Digest::MD5.new
-        if scope.to_hash.key?('server_facts')
-          unique_identifier.update scope['server_facts']['servername'] + facts['networking']['fqdn']
-          serverless = false
+      begin
+        version = metadata['version']
+        if version.to_s.empty?
+          Puppet.send('warning', "The module '#{module_name}' has no metadata.json. puppetstats cannot analyse data without a module version.")
         else
-          unique_identifier.update facts['networking']['fqdn']
-          serverless = true
-        end
+          uri = URI.parse('https://puppetstats.com')
+          http = Net::HTTP.new(uri.host, uri.port)
+          http.use_ssl = true
 
-        data = {
-            :system => unique_identifier,
-            :time => Time.now.to_i,
-            :version => version,
-            :data => {
-                :serverless => serverless,
-                :facterversion => facts['facterversion'],
-                :is_virtual => facts['is_virtual'],
-                :kernel => facts['kernel'],
-                :kernelmajversion => facts['kernelmajversion'],
-                :kernelrelease => facts['kernelrelease'],
-                :kernelversion => facts['kernelversion'],
-                :os => facts['os'],
-                :ruby => facts['ruby'],
-                :timezone => facts['timezone'],
-                :puppetversion => facts['puppetversion'],
-            }
-        }
+          request = Net::HTTP::Post.new("/api/modules/#{fqmn}")
 
-        request.body = data.to_json
+          scope = closure_scope
+          facts = scope['facts']
 
-        response = http.request(request)
-
-        Puppet.send('debug', "puppetstats HTTP request body:\n#{request.body}")
-        Puppet.send('debug', "puppetstats HTTP response code:\n#{response.code}")
-        Puppet.send('debug', "puppetstats HTTP response body:\n#{response.body}")
-
-        case response.code.to_i
-          when 200
-            Puppet.send('notice', "puppetstats anonymously tracked usage of module '#{fqmn}' on system '#{unique_identifier}'.")
-          when 503
-            Puppet.send('notice', "Seems like puppetstats.com is doing some maintainence. Skipping anonymous usage tracking of module '#{fqmn}' for this run.")
+          unique_identifier = Digest::MD5.new
+          if scope.to_hash.key?('server_facts')
+            unique_identifier.update scope['server_facts']['servername'] + facts['networking']['fqdn']
+            serverless = false
           else
-            Puppet.send('warning', "puppetstats anonymous usage tracking failed for module #{fqmn} with status code '#{response.code}':\n#{response.body}.")
+            unique_identifier.update facts['networking']['fqdn']
+            serverless = true
+          end
+
+          data = {
+              :system => unique_identifier,
+              :time => Time.now.to_i,
+              :version => version,
+              :data => {
+                  :serverless => serverless,
+                  :facterversion => facts['facterversion'],
+                  :is_virtual => facts['is_virtual'],
+                  :kernel => facts['kernel'],
+                  :kernelmajversion => facts['kernelmajversion'],
+                  :kernelrelease => facts['kernelrelease'],
+                  :kernelversion => facts['kernelversion'],
+                  :os => facts['os'],
+                  :ruby => facts['ruby'],
+                  :timezone => facts['timezone'],
+                  :puppetversion => facts['puppetversion'],
+              }
+          }
+
+          request.body = data.to_json
+
+          response = http.request(request)
+
+          Puppet.send('debug', "puppetstats HTTP request body:\n#{request.body}")
+          Puppet.send('debug', "puppetstats HTTP response code:\n#{response.code}")
+          Puppet.send('debug', "puppetstats HTTP response body:\n#{response.body}")
+
+          case response.code.to_i
+            when 200
+              Puppet.send('notice', "puppetstats anonymously tracked usage of module '#{fqmn}' on system '#{unique_identifier}'.")
+            when 503
+              Puppet.send('notice', "Seems like puppetstats.com is doing some maintainence. Skipping anonymous usage tracking of module '#{fqmn}' for this run.")
+            else
+              Puppet.send('warning', "puppetstats anonymous usage tracking failed for module #{fqmn} with status code '#{response.code}':\n#{response.body}.")
+          end
         end
+      rescue => error
+        Puppet.send('warning', "Could not track module '#{fqmn}' because of unknown error: #{error}")
       end
     end
   end
