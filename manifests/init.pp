4585ee072201f88cde29761afb480a4464b48613
kevin.schu <kevin.schu@innovation4web.de>
2018-09-22 13:11:38 +0200

    Show warning only when module is not disabled

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,10 @@
 define puppetstats (
   Boolean $enabled = true
 ) {
-  if $caller_module_name == '' {
-    warning('"puppetstats" should always be used within a module. Do not use is directly on your nodes.')
-  }
-
   if $facts['puppetstats_disabled'] == undef {
+    if $caller_module_name == '' {
+      warning('"puppetstats" should always be used within a module. Do not use is directly on your nodes.')
+    }
     if $enabled {
       puppetstats::track($title, $caller_module_name)
     }
